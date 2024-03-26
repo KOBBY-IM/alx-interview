648a587433d729804e2c9c217179f2e39dc7e7ce
KOBBY_IM <nyarkotheo2@gmail.com>
2024-03-26 13:54:05 +0000

    split functions

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,33 +1,52 @@
 #!/usr/bin/python3
-""" Log parsing script that reads from stdin and computes metrics based on
-certain conditions"""
+""" Log parsing script that reads from stdin and computes
+metrics based on certain conditions"""
+
+import sys
 
 
-file_size = 0
-codes = {}
+def parse_log_line(line):
+    """Parses a log line in the specified format"""
+    words = line.split()
+    if len(words) != 8:
+        return None, None
+    if words[2] != "GET" or words[3] != "/projects/260" or not words[5].isdigit() or not words[6].isdigit():
+        return None, None
+
+        return int(words[6]), words[7]
+    return None, None
+
+
+def update_metrics(file_size, codes, file_size_increment, code):
+    """Updates the metrics based on the parsed log line information"""
+    file_size += file_size_increment
+    if code in ref_codes:
+        codes[code] = codes.get(code, 0) + 1
+    return file_size, codes
+
+
+def print_stats(file_size, codes):
+    """Prints the total file size and number of lines by status code"""
+    print(f"Total file size: {file_size}")
+    for code in sorted(codes.keys()):
+        print(f"{code}: {codes[code]}")
+
+
 ref_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 count = 0
-if __name__ == "__main__":
-    import sys
+file_size = 0
+codes = {}
 
-    def print_out(file_info, codes_info):
-        """Prints information of files and status codes"""
-        print(f"File size: {file_info:d}")
-        for code in sorted(codes_info.keys()):
-            print(f"{code}: {codes_info[code]:d}")
-
-    try:
-        for line in sys.stdin:
-            words = line.split()
-            if len(words) == 9 and '/projects/260' in words:
-                file_size += int(words[-1])
-                if words[-2] in ref_codes:
-                    codes[words[-2]] = codes.get(words[-2], 0) + 1
-                count += 1
+try:
+    for line in sys.stdin:
+        file_size_increment, code = parse_log_line(line)
+        if file_size_increment is not None:
+            file_size, codes = update_metrics(file_size, codes,
+                                              file_size_increment, code)
+            count += 1
             if count == 10:
-                print_out(file_size, codes)
+                print_stats(file_size, codes)
                 count = 0
-        print_out(file_size, codes)
-    except KeyboardInterrupt:
-        print_out(file_size, codes)
-        raise
+except KeyboardInterrupt:
+    print_stats(file_size, codes)
+    raise
