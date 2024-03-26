f362f737fa470fb671da50e9ee6bc3653a630b9f
KOBBY_IM <nyarkotheo2@gmail.com>
2024-03-26 13:32:25 +0000

    fixed requirement and white spaces

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Log parsing script that reads from stdin and computes metrics based on certain conditions"""
+""" Log parsing script that reads from stdin and computes
+metrics based on certain conditions"""
 
 import sys
+
 
 def parse_log_line(line):
     """Parses a log line and returns relevant information"""
@@ -10,6 +12,7 @@ def parse_log_line(line):
         return int(words[-1]), words[-2]
     return None, None
 
+
 def update_metrics(file_size, codes, file_size_increment, code):
     """Updates the metrics based on the parsed log line information"""
     file_size += file_size_increment
@@ -17,11 +20,13 @@ def update_metrics(file_size, codes, file_size_increment, code):
         codes[code] = codes.get(code, 0) + 1
     return file_size, codes
 
+
 def print_out(file_info, codes_info):
     """Prints information of files and status codes"""
     print(f"File size: {file_info:d}")
     for code in sorted(codes_info.keys()):
         print(f"{code}: {codes_info[code]:d}")
+
 
 ref_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 count = 0
@@ -33,7 +38,8 @@ if __name__ == "__main__":
         for line in sys.stdin:
             file_size_increment, code = parse_log_line(line)
             if file_size_increment is not None:
-                file_size, codes = update_metrics(file_size, codes, file_size_increment, code)
+                file_size, codes = update_metrics(file_size, codes,
+                                                  file_size_increment, code)
                 count += 1
                 if count == 10:
                     print_out(file_size, codes)
