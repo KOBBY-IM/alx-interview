c684604f9757720a88eb1730e32f98913aec6637
KOBBY_IM <nyarkotheo2@gmail.com>
2024-03-26 13:35:01 +0000

    reverted to initial code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,33 @@
 #!/usr/bin/python3
-""" Log parsing script that reads from stdin and computes
-metrics based on certain conditions"""
-
-import sys
+""" Log parsing script that reads from stdin and computes metrics based on
+certain conditions"""
 
 
-def parse_log_line(line):
-    """Parses a log line and returns relevant information"""
-    words = line.split()
-    if len(words) == 9 and '/projects/260' in words:
-        return int(words[-1]), words[-2]
-    return None, None
-
-
-def update_metrics(file_size, codes, file_size_increment, code):
-    """Updates the metrics based on the parsed log line information"""
-    file_size += file_size_increment
-    if code in ref_codes:
-        codes[code] = codes.get(code, 0) + 1
-    return file_size, codes
-
-
-def print_out(file_info, codes_info):
-    """Prints information of files and status codes"""
-    print(f"File size: {file_info:d}")
-    for code in sorted(codes_info.keys()):
-        print(f"{code}: {codes_info[code]:d}")
-
-
-ref_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-count = 0
 file_size = 0
 codes = {}
-
+ref_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+count = 0
 if __name__ == "__main__":
+    import sys
+
+    def print_out(file_info, codes_info):
+        """Prints information of files and status codes"""
+        print(f"File size: {file_info:d}")
+        for code in sorted(codes_info.keys()):
+            print(f"{code}: {codes_info[code]:d}")
+
     try:
         for line in sys.stdin:
-            file_size_increment, code = parse_log_line(line)
-            if file_size_increment is not None:
-                file_size, codes = update_metrics(file_size, codes,
-                                                  file_size_increment, code)
+            words = line.split()
+            if len(words) == 9 and '/projects/260' in words:
+                file_size += int(words[-1])
+                if words[-2] in ref_codes:
+                    codes[words[-2]] = codes.get(words[-2], 0) + 1
                 count += 1
-                if count == 10:
-                    print_out(file_size, codes)
-                    count = 0
+            if count == 10:
+                print_out(file_size, codes)
+                count = 0
+        print_out(file_size, codes)
     except KeyboardInterrupt:
         print_out(file_size, codes)
         raise
